e7ec5e137bedc79dfb01e589c08d1362bba20fe9
Guilherme Macedo <gleo31294@gmail.com>
2024-07-15 10:41:55 -0300

    ;

--- js/script.js
+++ js/script.js
@@ -1,28 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const burger = document.getElementById("burger");
-    const menu = document.getElementById("menu");
-    const menuList = document.getElementById("menu-list");
-
-    // Função para alternar o menu
-    const toggleMenu = () => {
-        menu.classList.toggle("active");
-    };
-
-    // Evento de clique no botão burger
-    burger.addEventListener("click", toggleMenu);
-
-    // Evento de clique fora do menu para fechar
-    document.addEventListener("click", function (event) {
-        const isClickInsideMenu = menu.contains(event.target);
-        if (!isClickInsideMenu) {
-            menu.classList.remove("active");
-        }
-    });
-
-    // Evento de clique nos links do menu para fechar
-    menuList.addEventListener("click", function (event) {
-        if (event.target.tagName === "A") {
-            menu.classList.remove("active");
-        }
-    });
-});
+document.getElementById('burguer').addEventListener('click', () => {
+    const menu = document.getElementById('menu-list');
+  
+    if(menu.style.display == 'none'){
+        menu.style.display = 'flex'
+    }else{
+        menu.style.display ='none'
+    }
+  });
+  
